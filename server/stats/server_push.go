7bc0a1d489fc6a6d3b1f363871ec9f9d90bf6a43
Tit Petric <black@scene-si.org>
2019-12-13 21:31:49 +0100

    import server push implementation

---
+++ server/stats/server_push.go
@@ -0,0 +1,34 @@
+package stats
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/titpetric/microservice/rpc/stats"
+)
+
+// Push a record to the incoming log table
+func (svc *Server) Push(ctx context.Context, r *stats.PushRequest) (*stats.PushResponse, error) {
+	var err error
+	row := Incoming{}
+
+	row.ID, err = svc.sonyflake.NextID()
+	if err != nil {
+		return nil, err
+	}
+
+	row.Property = r.Property
+	row.PropertySection = r.Section
+	row.PropertyID = r.Id
+	row.RemoteIP = "127.0.0.1"
+	row.SetStamp(time.Now())
+
+	fields := strings.Join(IncomingFields, ",")
+	named := ":" + strings.Join(IncomingFields, ",:")
+
+	query := fmt.Sprintf("insert into %s (%s) values (%s)", IncomingTable, fields, named)
+	_, err = svc.db.NamedExecContext(ctx, query, row)
+	return nil, err
+}
