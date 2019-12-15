5100e97fb1ba477ba53f77e2298013e0185db8ba
Tit Petric <black@scene-si.org>
2019-12-15 13:45:50 +0100

    rename config fields to lowercase

--- cmd/db-migrate-cli/main.go
+++ cmd/db-migrate-cli/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"flag"
 	"context"
+	"flag"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -13,33 +13,33 @@ import (
 func main() {
 	var config struct {
 		db      db.ConnectionOptions
-		Real    bool
-		Service string
+		real    bool
+		service string
 	}
 	flag.StringVar(&config.db.Credentials.Driver, "db-driver", "mysql", "Database driver")
 	flag.StringVar(&config.db.Credentials.DSN, "db-dsn", "", "DSN for database connection")
-	flag.StringVar(&config.Service, "service", "", "Service name for migrations")
-	flag.BoolVar(&config.Real, "real", false, "false = print migrations, true = run migrations")
+	flag.StringVar(&config.service, "service", "", "Service name for migrations")
+	flag.BoolVar(&config.real, "real", false, "false = print migrations, true = run migrations")
 	flag.Parse()
 
-	if config.Service == "" {
+	if config.service == "" {
 		log.Printf("Available migration services: %+v", db.List())
 		log.Fatal()
 	}
 
 	ctx := context.Background()
 
-	switch config.Real {
+	switch config.real {
 	case true:
 		handle, err := db.ConnectWithRetry(ctx, config.db)
 		if err != nil {
 			log.Fatalf("Error connecting to database: %+v", err)
 		}
-		if err := db.Run(config.Service, handle); err != nil {
+		if err := db.Run(config.service, handle); err != nil {
 			log.Fatalf("An error occured: %+v", err)
 		}
 	default:
-		if err := db.Print(config.Service); err != nil {
+		if err := db.Print(config.service); err != nil {
 			log.Fatalf("An error occured: %+v", err)
 		}
 	}
