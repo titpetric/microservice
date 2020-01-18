5f5e3a647118d39d1ccd7fbd2e5361e188a3947d
Tit Petric <black@scene-si.org>
2020-01-18 12:49:30 +0100

    add comments

--- server/stats/pool.go
+++ server/stats/pool.go
@@ -4,17 +4,20 @@ import (
 	"sync"
 )
 
+// Pool provides a queuing structure for Incoming{}
 type Pool struct {
 	sync.RWMutex
 	values []*Incoming
 }
 
+// NewPool creates a new *Pool instance
 func NewPool() *Pool {
 	return &Pool{
 		values: make([]*Incoming, 0),
 	}
 }
 
+// NewPools creates a slice of *Pool instances
 func NewPools(size int) []*Pool {
 	result := make([]*Pool, size)
 	for i := 0; i < size; i++ {
@@ -23,6 +26,7 @@ func NewPools(size int) []*Pool {
 	return result
 }
 
+// Clear returns current pool items and clears it
 func (p *Pool) Clear() (result []*Incoming) {
 	length := p.Length()
 
@@ -33,12 +37,14 @@ func (p *Pool) Clear() (result []*Incoming) {
 	return
 }
 
+// Push adds a new item to the pool
 func (p *Pool) Push(item *Incoming) {
 	p.Lock()
 	defer p.Unlock()
 	p.values = append(p.values, item)
 }
 
+// Length returns the current pool size
 func (p *Pool) Length() int {
 	p.RLock()
 	defer p.RUnlock()
