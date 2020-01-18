4798cc6ca169bc78c126dedca88f610fd7ffaef6
Tit Petric <black@scene-si.org>
2020-01-18 13:42:50 +0100

    implement queue flushing

--- server/stats/flusher.go
+++ server/stats/flusher.go
@@ -2,7 +2,9 @@ package stats
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -16,18 +18,38 @@ type Flusher struct {
 
 	enabled *atomic.Bool
 
+	// queueIndex is a key for []queues
+	queueIndex *atomic.Uint32
+	// queueMask is a masking value for queueIndex -> key
+	queueMask uint32
+	// queues hold a set of writable queues
+	queues []*Queue
+
 	db *sqlx.DB
 }
 
 // NewFlusher creates a *Flusher
 func NewFlusher(ctx context.Context, db *sqlx.DB) (*Flusher, error) {
+	queueSize := 1 << 4
 	job := &Flusher{
-		db:      db,
-		enabled: atomic.NewBool(true),
+		db:         db,
+		enabled:    atomic.NewBool(true),
+		queueIndex: atomic.NewUint32(0),
+		queueMask:  uint32(queueSize - 1),
+		queues:     NewQueues(queueSize),
 	}
 	job.Context, job.finish = context.WithCancel(context.Background())
 	go job.run(ctx)
 	return job, nil
+}
+
+// Push spreads queue writes evenly across all queues
+func (job *Flusher) Push(item *Incoming) error {
+	if job.enabled.Load() {
+		index := job.queueIndex.Inc() & job.queueMask
+		return job.queues[index].Push(item)
+	}
+	return errFlusherDisabled
 }
 
 func (job *Flusher) run(ctx context.Context) {
@@ -35,7 +57,7 @@ func (job *Flusher) run(ctx context.Context) {
 
 	defer job.finish()
 
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(5 * time.Second)
 
 	for {
 		select {
@@ -54,5 +76,28 @@ func (job *Flusher) run(ctx context.Context) {
 }
 
 func (job *Flusher) flush() {
-	log.Println("Background flush")
+	var err error
+
+	fields := strings.Join(IncomingFields, ",")
+	named := ":" + strings.Join(IncomingFields, ",:")
+	query := fmt.Sprintf("insert into %s (%s) values (%s)", IncomingTable, fields, named)
+
+	var batchInsertSize int
+	for k, queue := range job.queues {
+		rows := queue.Clear()
+
+		for len(rows) > 0 {
+			batchInsertSize = 1000
+			if len(rows) < batchInsertSize {
+				batchInsertSize = len(rows)
+			}
+			log.Println("[flush] queue", k, "remaining", len(rows))
+			if _, err = job.db.NamedExec(query, rows[:batchInsertSize]); err != nil {
+				log.Println("Error when flushing data:", err)
+			}
+			rows = rows[batchInsertSize:]
+		}
+	}
+
+	log.Println("[flush] done")
 }
